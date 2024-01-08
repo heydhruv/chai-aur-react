2ac60a0af6c18e3587a235c96e175e45cb3e3a66
Dhruv Dave <heydhruvdave@gmail.com>
2024-01-09 04:19:19 +0530

    everything is almost done only lord debugging remains

--- blogger/src/components/index.js
+++ blogger/src/components/index.js
@@ -4,7 +4,12 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-
+import Signup from './Signup'
+import Postcard from "./Postcard"
+import Login from "./Login"
+import Button from "./Button"
+import PostForm from "./PostForm/PostForm"
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
@@ -12,4 +17,10 @@ export {
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Postcard,
+    Login,
+    Button,
+    PostForm,
+    AuthLayout,
 }
